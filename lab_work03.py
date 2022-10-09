774b6ef4ec68f483dfa6c894ff2b4be57fe50b06
xXPro100KillerXx <113775345+xXPro100KillerXx@users.noreply.github.com>
2022-10-09 22:57:02 +0500

    Add files via upload

--- lab_work03.py
+++ lab_work03.py
@@ -10,37 +10,106 @@ print(alph)
 print(alphr)
 
 
-x = str(input(('Введите слово, которое нужно зашифровать: ')))
-step = int(input('Введите шаг(число) шифрования: '))
-resolt = ''
-tloser = ''
+try:
+    try:
+        num = int(input('выберете операцию: зашифровка - 1, расшифровка - 2: '))
+        a = 0
+        resolt = ''
+        tloser = ''
+        if num == 1:
+            x = str(input(('Введите пердложение, которое нужно зашифровать: ')))
+            step = int(input('Введите шаг(число) шифрования: '))
+
+            while step > 33:
+                step -= 33
+            k = step
+
+            for i in range(0, len(x)):
+                if x[i] == ' ':
+                    resolt += ' '
+                else:
+                    m = alph[x[i]]
+                    if m + k <= 33:
+                        resolt += alphr[m + k]
+                    else:
+                        resolt += alphr[(m + k) - 33]
+            print(resolt)
+
+        elif num == 2:
+            x = str(input(('Введите пердложение, которое нужно расшифровать: ')))
+            step = int(input('Введите шаг(число) расшифровки: '))
+
+            while step > 33:
+                step -= 33
+            k = step
+
+            for i in range(0, len(x)):
+                if x[i] == ' ':
+                    tloser += ' '
+                else:
+                    m = alph[x[i]]
+                    if m - k > 0:
+                        tloser += alphr[m - k]
+                    else:
+                        tloser += alphr[(m - k) + 33]
+            print(tloser)
+
+    except KeyError:
+        a = 1
+        print('Ошибка, Вы ввели не слово')
+except ValueError:
+     a = 1
+     print('Ошибка, Вы ввели не цифру')
 
 
-while step > 33:
-    step -= 33
-k = step
+while a == 1:
+    try:
+        try:
+            num = int(input('выберете операцию: зашифровка - 1, расшифровка - 2: '))
+            a = 0
+            resolt = ''
+            tloser = ''
+            if num == 1:
+                x = str(input(('Введите пердложение, которое нужно зашифровать: ')))
+                step = int(input('Введите шаг(число) шифрования: '))
 
-for i in range(0, len(x)):
-    if x[i] == ' ':
-        resolt += ' '
-    else:
-        m = alph[x[i]]
-        if m + k <= 33:
-            resolt += alphr[m + k]
-        else:
-            resolt += alphr[(m + k) - 33]
+                while step > 33:
+                    step -= 33
+                k = step
 
+                for i in range(0, len(x)):
+                    if x[i] == ' ':
+                        resolt += ' '
+                    else:
+                        m = alph[x[i]]
+                        if m + k <= 33:
+                            resolt += alphr[m + k]
+                        else:
+                            resolt += alphr[(m + k) - 33]
+                print(resolt)
 
+            elif num == 2:
+                x = str(input(('Введите пердложение, которое нужно расшифровать: ')))
+                step = int(input('Введите шаг(число) расшифровки: '))
 
-for i in range(0, len(resolt)):
-    if x[i] == ' ':
-        tloser += ' '
-    else:
-        m = alph[resolt[i]]
-        if m - k > 0:
-            tloser += alphr[m - k]
-        else:
-            tloser += alphr[(m - k) + 33]
+                while step > 33:
+                    step -= 33
+                k = step
 
-print(resolt)
-print(tloser)
+                for i in range(0, len(x)):
+                    if x[i] == ' ':
+                        tloser += ' '
+                    else:
+                        m = alph[x[i]]
+                        if m - k > 0:
+                            tloser += alphr[m - k]
+                        else:
+                            tloser += alphr[(m - k) + 33]
+                print(tloser)
+
+        except KeyError:
+            a = 1
+            print('Ошибка, Вы ввели не слово')
+    except ValueError:
+        a = 1
+        print('Ошибка, Вы ввели не цифру')
